16b151ae16215e4d118081bbd1f752fe0979b66e
adelia <103928715+sssamatovna@users.noreply.github.com>
2025-11-07 14:56:05 +0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,12 +1,12 @@
 import pytest
+import random
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options 
 from webdriver_manager.chrome import ChromeDriverManager
-from sdet_practikum.pages.base_page import BasePage
-from sdet_practikum.pages.manager_page import ManagerPage
-from sdet_practikum.utils import random_data
-import random
-import allure
+from pages.manager_page import ManagerPage
+from pages.customers_page import CustomersPage
+from utils import data_generator
 
 
 @pytest.fixture(scope="session")
@@ -16,32 +16,49 @@ def base_url():
 
 @pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    driver.maximize_window()
-    driver.implicitly_wait(10)
+    options = Options()
+  
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+    
+    
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()),
+        options=options 
+    )
+    
+    driver.implicitly_wait(10) 
+  
     yield driver
     driver.quit()
 
 
 @pytest.fixture(scope="function")
 def create_test_customers(driver, base_url):
+    """
+    Фикстура для создания трех клиентов с именами, имеющими длины 5, 7 и 9.
+    Возвращает список этих сгенерированных имен.
+    """
     manager_page = ManagerPage(driver, base_url, path="/#/manager")
     manager_page.open()
+    
 
     required_lengths = [5, 7, 9]
     names_to_add = []
-
+    
     with allure.step(f"Создание клиентов с длинами имен {required_lengths}"):
         for length in required_lengths:
-            # Используем новую функцию для генерации имени нужной длины
-            name = random_data.generate_name_by_length(length)
 
-            # Добавляем клиента
+            name = data_generator.generate_name_by_length(length)
+            
+
             manager_page.go_to_add_customer()
-            # Используем случайный, но уникальный Post Code
-            post_code = str(random.randint(1000000000, 9999999999))
-            manager_page.add_customer(name, "Sdet", post_code)
 
+            post_code = str(random.randint(1000000000, 9999999999))
+            manager_page.add_customer(name, "GenTest", post_code)
+            
             names_to_add.append(name)
 
     return names_to_add
